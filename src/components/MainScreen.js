a40e1ddbdd5bcdc3ece01f713fbaa0554fb047bd
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2024-01-08 15:00:52 +0300

    1

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -22,62 +22,6 @@ const MainScreen = ({
   go,
 }) => {
 
-  useEffect(() => {
-    const sendRequestToBackend = async (accessToken) => {
-      const url = 'https://valentine.itc-hub.ru/api/v1/createuser';
-
-      const headers = {
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-      };
-
-      const requestBody = {
-        // 
-      };
-
-      try {
-        const response = await fetch(url, {
-          method: 'POST',
-          headers,
-          body: JSON.stringify(requestBody),
-        });
-
-        if (response.ok) {
-          console.log('User created successfully!');
-          // действия после успешного создания пользователя
-        } else {
-          console.error('Failed to create user:', response.statusText);
-        }
-      } catch (error) {
-        console.error('Error creating user:', error.message);
-      }
-    };
-
-    const clientId = '51826188'; // ID приложения
-    const redirectUri = 'https://yourdomain.com/auth/vk/callback'; // Зарегистрированный redirect URI
-
-    const getAccessTokenFromUrl = () => {
-      const hashParams = window.location.hash.substr(1).split('&');
-      const params = hashParams.reduce((acc, param) => {
-        const [key, value] = param.split('=');
-        acc[key] = value;
-        return acc;
-      }, {});
-      return params.access_token;
-    };
-
-    const accessToken = getAccessTokenFromUrl();
-
-    if (accessToken) {
-      //отправика Access Token на сервер 
-      console.log('Access Token:', accessToken);
-
-      // Отправляем запрос на бэкенд для создания пользователя
-      sendRequestToBackend(accessToken);
-    } else {
-      window.location.href = `https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&v=5.131`;
-    }
-  }, []);
 
 
   return (
--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import { Panel, PanelHeader, Group, Div, Button, Progress } from '@vkontakte/vkui';
 import '../styles/Tutorial.css';
 
 
 const Tutorial = ({ id, tutorialStep, nextTutorialStep, go }) => {
+
   const renderTutorialContent = () => {
+
+    useEffect(() => {
+      const sendRequestToBackend = async (accessToken) => {
+        const url = 'https://valentine.itc-hub.ru/api/v1/createuser';
+  
+        const headers = {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        };
+  
+        const requestBody = {
+          // 
+        };
+  
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(requestBody),
+          });
+  
+          if (response.ok) {
+            console.log('User created successfully!');
+            // действия после успешного создания пользователя
+          } else {
+            console.error('Failed to create user:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error creating user:', error.message);
+        }
+      };
+  
+      const clientId = '51826188'; // ID приложения
+      const redirectUri = 'https://yourdomain.com/auth/vk/callback'; // Зарегистрированный redirect URI
+  
+      const getAccessTokenFromUrl = () => {
+        const hashParams = window.location.hash.substr(1).split('&');
+        const params = hashParams.reduce((acc, param) => {
+          const [key, value] = param.split('=');
+          acc[key] = value;
+          return acc;
+        }, {});
+        return params.access_token;
+      };
+  
+      const accessToken = getAccessTokenFromUrl();
+  
+      if (accessToken) {
+        //Отправляем Access Token на сервер 
+        console.log('Access Token:', accessToken);
+  
+        // Отправляем запрос на бэкенд для создания пользователя
+        sendRequestToBackend(accessToken);
+      } else {
+        window.location.href = `https://oauth.vk.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&v=5.131`;
+      }
+    }, []);
+
     switch (tutorialStep) {
       case 1:
         return (
